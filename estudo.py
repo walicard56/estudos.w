6adb77ac3cefd41013bc78073d1287f138473f56
Walisson Jose <walissoncair@gmail.com>
2026-05-31 10:13:32 -0300

    branch nova

---
+++ estudo.py
@@ -0,0 +1,7 @@
+print("Hello, World!" \
+"This is a multi-line string." \
+    "Python is great for learning programming.")
+
+# This is a single-line comment
+# The following line of code will print a message to the console
+print("AGORA ESTOU NA BRANCH NOVA")  # This line will cause an error because 'PRINT' should be lowercase 'print'
