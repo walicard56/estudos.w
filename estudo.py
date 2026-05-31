4406a26cf9ad0352247c13825dda1bebff5de1be
Walisson Jose <walissoncair@gmail.com>
2026-05-31 11:25:32 -0300

    feat(greet): add function to get name

--- estudo.py
+++ estudo.py
@@ -5,3 +5,9 @@ print("Hello, World!" \
 # This is a single-line comment
 # The following line of code will print a message to the console
 print("AGORA ESTOU NA BRANCH NOVA")  # This line will cause an error because 'PRINT' should be lowercase 'print'
+
+def greet(name):
+    """This function takes a name as input and returns a greeting message."""
+    return f"Hello, {name}!"
+
+print(greet("ana"))  # This will print the greeting message for 'ana'
