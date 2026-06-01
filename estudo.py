ecdfccd07570c31da7df1e91007b399b8c875111
Walisson Jose <walissoncair@gmail.com>
2026-06-01 11:25:01 -0300

    feat(decorators) function

--- estudo.py
+++ estudo.py
@@ -1,3 +1,12 @@
-print('Hello, World!')
-print('Welcome to Python programming.')
-print('minha nova branch')
+def decorador(funcao):
+    def nova_funcao():
+        print("Antes da função original")
+        funcao()
+        print("Depois da função original")
+    return nova_funcao
+
+@decorador
+def minha_funcao():
+    print("Esta é a função original")
+
+minha_funcao()
