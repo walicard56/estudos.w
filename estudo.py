ffaabd275cd2af7bfacf9c869f4048323776cab2
Walisson Jose <walissoncair@gmail.com>
2026-06-01 11:14:55 -0300

    feat(função) add def saudades and executar in my estudo.py

--- estudo.py
+++ estudo.py
@@ -1,13 +1,7 @@
-print("Hello, World!" \
-"This is a multi-line string." \
-    "Python is great for learning programming.")
+def saudades(nome):
+    return f"Saudades de você, {nome}!"
 
-# This is a single-line comment
-# The following line of code will print a message to the console
-print("AGORA ESTOU NA BRANCH NOVA")  # This line will cause an error because 'PRINT' should be lowercase 'print'
+def executar(func, arg):
+    print(func(arg))
 
-def greet(name):
-    """This function takes a name as input and returns a greeting message."""
-    return f"Hello, {name}!"
-
-print(greet("ana"))  # This will print the greeting message for 'ana'
+executar(saudades, "Maria")
