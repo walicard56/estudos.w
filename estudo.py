b130d53684e51324a9d9f9f4bf3d0fae86b32ebb
Walisson Jose <walissoncair@gmail.com>
2026-06-01 11:33:16 -0300

    feat(closures and decorators) functions

--- estudo.py
+++ estudo.py
@@ -1,7 +1,24 @@
 def saudades(nome):
     return f"Saudades de você, {nome}!"
 
+def idade(ano_nascimento):
+    from datetime import datetime
+    ano_atual = datetime.now().year
+    anos = ano_atual - ano_nascimento
+    return f"voce tem {anos} anos de idade."
+
+def doença(temperatura):
+    def sintomas():
+        return "Você pode estar com gripe."
+
+    if temperatura > 38:
+        print(f"Você está com febre.({temperatura}°C)")
+
+    return sintomas()
+
 def executar(func, arg):
     print(func(arg))
 
 executar(saudades, "Maria")
+executar(idade, 1990)
+executar(doença, 39)
